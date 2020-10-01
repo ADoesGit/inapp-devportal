236551c1b5bf5716e2ee69a3ef81fb12f9ad9076
AAGaming <alex.bonasia@gmail.com>
2020-09-30 22:57:31 -0400

    e

--- index.js
+++ index.js
@@ -10,24 +10,54 @@ const { inject, uninject } = require('powercord/injector');
 module.exports = class InAppDevPortal extends Plugin {
   startPlugin () {
     this._injectDevPortal();
-    this.registerRoute('/inapp-devportal', () => React.createElement('iframe', {
-      src: DEVELOPER_PORTAL,
-      style: {
-        width: '100%',
-        height: '100%'
+    powercord.api.router.registerRoute({
+      path: '/inapp-devportal',
+      render: () => React.createElement('iframe', {
+        className: 'inappdevportal',
+        src: DEVELOPER_PORTAL,
+        style: {
+          width: '100%',
+          height: '100%'
+        }
+      }),
+      noSidebar: false
+    });
+    powercord.api.commands.registerCommand({
+      command: 'devportal',
+      description: 'Temp command to go to devportal until i fix the button.',
+      usage: '{c}',
+      executor: async () => {
+        require('powercord/webpack').getModule([ 'transitionTo' ], false).transitionTo('/_powercord/inapp-devportal');
+        const doc = document.querySelector('.inappdevportal').contentDocument;
+        Array.prototype.forEach.call(document.querySelectorAll('link[rel=stylesheet]'), (link) => {
+          const newLink = document.createElement('link');
+          newLink.rel = link.rel;
+          newLink.href = link.href;
+          doc.head.appendChild(newLink);
+        });
+        Array.prototype.forEach.call(document.querySelectorAll('style'), (link) => {
+          const newLink = document.createElement('style');
+          newLink.innerHTML = link.innerHTML;
+          doc.head.appendChild(newLink);
+        });
       }
-    }));
+    });
   }
 
   pluginWillUnload () {
     uninject('devportal-item');
+    powercord.api.router.unregisterRoute('/inapp-devportal');
+    powercord.api.commands.unregisterCommand('devportal');
   }
 
   async _injectDevPortal () {
     const PrivateChannel = await getModule([ 'LinkButton' ]);
-    const PrivateChannelsList = await getModuleByDisplayName('FluxContainer(PrivateChannelsList)');
-    inject('devportal-item', PrivateChannelsList.prototype, 'render', (_, res) => {
+    // const PrivateChannelsList = await getModuleByDisplayName('ConnectedPrivateChannelsList');
+    const PrivateChannelsList = await getModule(m => m.default && m.default.displayName === 'ConnectedPrivateChannelsList');
+    console.log(PrivateChannel, PrivateChannelsList);
+    inject('devportal-item', PrivateChannelsList, 'default', (_, res) => {
       const selected = window.location.pathname === '/_powercord/inapp-devportal';
+      console.log(res);
       const index = res.props.children.map(c => c && c.type && c.type.displayName && c.type.displayName.includes('FriendsButtonInner')).indexOf(true) + 1;
       if (selected) {
         res.props.children.forEach(c => {
@@ -36,15 +66,17 @@ module.exports = class InAppDevPortal extends Plugin {
       }
       res.props.children = [
         ...res.props.children.slice(0, index),
-        React.createElement(PrivateChannel.LinkButton, {
+        () => React.createElement('a', {
           iconName: 'OverlayOn',
           route: '/_powercord/inapp-devportal',
           text: 'Developer Portal',
           selected
         }),
-        res.props.children.slice(index)
+        ...res.props.children.slice(index)
       ];
+      // delete res.props.children[32]
       return res;
     });
+    PrivateChannelsList.default.displayName = 'ConnectedPrivateChannelsList';
   }
 };
